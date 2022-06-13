f138478db2f3921e82fd03b5e992042e793a5cad
JakubJarosz <jakubtm165@gmail.com>
2022-06-13 15:50:03 +0200

    fix

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+import throttle from 'lodash.throttle';
 
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+let interval = 0;
+stopButton.disabled = true
+
+const handleclickStart = () => {
+    interval = setInterval(function () {
+      const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+      document.body.style.backgroundColor = '#' + randomColor;
+    }, 1000);
+    startButton.disabled = true
+    stopButton.disabled = false
+};
+
+const handleclickStop = () => {
+    clearInterval(interval);
+    stopButton.disabled = true
+    startButton.disabled = false
+}
+
+startButton.addEventListener("click", handleclickStart);
+
+stopButton.addEventListener('click', handleclickStop);
